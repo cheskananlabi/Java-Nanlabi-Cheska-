32c7f13e8ed558931186f690004cdfdb6a062d45
cheyyziii <nanlabicheska916@gmail.com>
2026-06-17 14:42:21 +0800

    bwerbargba

---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,33 @@
+package Basic_Structure_and_Syntax;
+
+public class HelloWorld {
+    public static void HelloWorld(String[] args) {
+        /* System.out.println
+        ("Hello, World!");
+        System.out.println
+        ("Welcome to Java programming.");
+        System.out.println
+        ("I am Cheska L. Nanlabi, ICT-Programming student.");
+        System.out.println
+        (136635140361l);
+        System.out.println
+        ("I am 16 years old.");
+        System.out.println
+        ("I came from my mothers womb.");
+        System.out.println
+        ("Happy coding!");
+        */
+       
+        System.out.print ("Addition: ");
+        System.out.println ("6 + 7 = " + (6 + 7)); // This is addition
+
+        System.out.print ("Subtraction: ");
+        System.out.println ("6 - 7 = " + (6 - 7)); // This is subtraction
+
+        System.out.print ("Multiplication: ");
+        System.out.println ("6 * 7 = " + (6 * 7)); // This is multiplication
+
+        System.out.print ("Division: ");
+        System.out.println ("6 / 7 = " + (6 / 7)); // This is division, but since both 6 and 7 are integers, it will perform integer division and return 0  
+    }
+}
