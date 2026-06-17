f00c2b14cb09fdc6381af5eb9e1d6cb2feff42cf
cheyyziii <nanlabicheska916@gmail.com>
2026-06-17 14:16:12 +0800

    commitment

---
+++ Basic_Structure_and_Syntax/main.java
@@ -0,0 +1,28 @@
+package Basic_Structure_and_Syntax;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println
+        ("Hello, World!");
+        System.out.println
+        ("Welcome to Java programming.");
+        System.out.println
+        ("I am Cheska L. Nanlabi, ICT-Programming student.");
+        System.out.println
+        (136635140361l);
+        System.out.println
+        ("I am 16 years old.");
+        System.out.println
+        ("I am from my mothers womb.");
+        System.out.println
+        ("Happy coding!");
+        System.out.println
+        ("Addition: " + (6 + 7));
+        System.out.println
+        ("Subtraction: " + (6 - 7));
+        System.out.println
+        ("Multiplication: " + (6 * 7));
+        System.out.println
+        ("Division: " + (6 / 7));
+    }
+}
