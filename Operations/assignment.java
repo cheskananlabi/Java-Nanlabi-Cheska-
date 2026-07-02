0b982ad3215a2e8a18bfed183b60c8ddb2d9854f
cheyyziii <nanlabicheska916@gmail.com>
2026-07-02 21:43:44 +0800

    grr

---
+++ Operations/assignment.java
@@ -0,0 +1,14 @@
+package Operations;
+
+public class assignment {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a += 5);
+        System.out.println(a -= 3);
+        System.out.println(a *= 2);
+        System.out.println(a /= 4);
+        System.out.println(a %= 4);
+        int x = 19;
+        System.out.println(x % 5);
+    }
+}
