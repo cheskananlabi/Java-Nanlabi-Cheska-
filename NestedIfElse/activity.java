e69329f8724c71248ff8ed6077600db154eada99
cheyyziii <nanlabicheska916@gmail.com>
2026-08-03 07:59:36 +0800

    jttykhtjr

---
+++ NestedIfElse/activity.java
@@ -0,0 +1,12 @@
+package NestedIfElse;
+
+import java.util.Scanner;
+
+public class activity {
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("MENU");
+    
+    }
+}
